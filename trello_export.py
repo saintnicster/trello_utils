986ff550ca0b35f23135299a8ae9ff1285c0bbe3
Nick Fajardo <Nick.Fajardo@conocophillips.com>
2017-05-04 16:31:21 -0500

    Changed module used to access Trello

--- trello_export.py
+++ trello_export.py
@@ -1,4 +1,4 @@
-from trello import TrelloApi
+from trello import TrelloClient
 from pprint import pprint
 from inspect import getmembers
 from Mytrello import Mytrello
@@ -6,45 +6,47 @@ from datetime import datetime
 import json
 import csv
 
-TRELLO_APP_KEY = "***REMOVED***"
-TOKEN = "***REMOVED***"
+client = TrelloClient(
+    api_key='***REMOVED***',
+    api_secret='***REMOVED***',
+    token='cf7bc847fedeedd732c113b2f6792dbcb8ac97df62498a8a8ba58a16655a54ee',
+    token_secret='***REMOVED***'
+)
+
 ISSOW_BOARD_ID = '587d4739aacd90eb33fedfce'
 
 list_lookup = {}
 csv_output = []
+issow_board = client.get_board(ISSOW_BOARD_ID)
+issow_lists = issow_board.all_lists( )
+all_cards = issow_board.open_cards( )
 
-trello = TrelloApi(TRELLO_APP_KEY, TOKEN)
-trello.extras = Mytrello(TRELLO_APP_KEY, TOKEN)
-
-issow_lists = trello.boards.get_list(ISSOW_BOARD_ID)
 for list in issow_lists:
-    list_lookup[list['id']] = list
+    list_lookup[list.id] = list
 
-issow_cards = trello.boards.get_card(ISSOW_BOARD_ID,None,None,None,None,None,None,"idList,name,url,labels,closed")
-
-for card in issow_cards:
-    if card["closed"] == True:
-        print("Card archived! %s" % card['name'])
+for card in all_cards:
+    if card.closed == True:
+        print("Card archived! %s" % card.name)
         continue
     
-    card["labelText"] = card["ServiceNow_Status"] = card["ServiceNow_Assigned"] = card["ServiceNow_BU"] = card["BU_Identified_Priority"] = ""
+    card.labelText = card.ServiceNow_Status = card.ServiceNow_Assigned = card.ServiceNow_BU = card.BU_Identified_Priority = ""
     
-    for label in card["labels"]:
-        if label['color'] == 'orange':
-           card["ServiceNow_Status"] = label['name'] 
-        elif label['color'] == 'blue':
-            card["ServiceNow_Assigned"] = label['name'] 
-        elif label['color'] == 'lime':
-            card["ServiceNow_BU"] = label['name'] 
-        elif label['color'] == 'red':
-            card["BU_Identified_Priority"] = 'YES'
+    for label in card.list_labels:
+        if label.color == 'orange':
+           card.ServiceNow_Status = label.name 
+        elif label.color == 'blue':
+            card.ServiceNow_Assigned = label.name 
+        elif label.color == 'lime':
+            card.ServiceNow_BU = label.name 
+        elif label.color == 'red':
+            card.BU_Identified_Priority = 'YES'
     
-    csv_output.append({ "incidentName": card['name'],
-                        "listName": list_lookup[card['idList']]['name'],
-                        "BU_Reported": card["ServiceNow_BU"],
-                        "SN_Status": card["ServiceNow_Status"],
-                        "SN_Assigned": card["ServiceNow_Assigned"],
-                        "BU_Identified_Priority": card["BU_Identified_Priority"] })
+    csv_output.append({ "incidentName": card.name,
+                        "listName": list_lookup[card.idList].name,
+                        "BU_Reported": card.ServiceNow_BU,
+                        "SN_Status": card.ServiceNow_Status,
+                        "SN_Assigned": card.ServiceNow_Assigned,
+                        "BU_Identified_Priority": card.BU_Identified_Priority })
 
 csv_filename = "//conoco.net/bvl_shared/temp/fajarn/trello_export_"+datetime.now().strftime("%Y%m%d%H%M%S")+'.csv'
 with open(csv_filename,'w') as csv_file:

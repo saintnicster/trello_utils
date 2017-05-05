15a86b11f81fe5db01b7f13305c7bfec30afbbc6
Nick Fajardo <Nick.Fajardo@conocophillips.com>
2017-05-05 10:54:10 -0500

    New module for Trello API

--- match_trello_to_SN.py
+++ match_trello_to_SN.py
@@ -1,4 +1,4 @@
-from trello import TrelloApi
+from trello import TrelloClient
 from pprint import pprint
 from inspect import getmembers
 from Mytrello import Mytrello
@@ -18,8 +18,12 @@ def read_servicenow_file():
             sn_dict[row['number']] = row
     return { 'rows' : file_rows, 'dict' : sn_dict }
 
-TRELLO_APP_KEY = "***REMOVED***"
-TOKEN = "***REMOVED***"
+client = TrelloClient(
+    api_key='***REMOVED***',
+    api_secret='***REMOVED***',
+    token='cf7bc847fedeedd732c113b2f6792dbcb8ac97df62498a8a8ba58a16655a54ee',
+    token_secret='***REMOVED***'
+)
 ISSOW_BOARD_ID = '587d4739aacd90eb33fedfce'
 CLOSED_LIST_ID = '587fb500aa3ee04f092ccc7d'
 
@@ -28,26 +32,26 @@ list_lookup = {}
 
 servicenow_dump = read_servicenow_file()
 
-trello = TrelloApi(TRELLO_APP_KEY, TOKEN)
-trello.extras = Mytrello(TRELLO_APP_KEY, TOKEN)
+issow_board = client.get_board(ISSOW_BOARD_ID)
+issow_lists = issow_board.all_lists()
 
-issow_lists = trello.boards.get_list(ISSOW_BOARD_ID)
 for list in issow_lists:
-    list_lookup[list['id']] = list
+    list_lookup[list.id] = list
 
-issow_cards = trello.boards.get_card(ISSOW_BOARD_ID,None,None,None,None,None,"all","idList,name,url,labels,closed")
-for card in issow_cards:
-    incident_number = card['name'].partition('-')[0].strip()
+#issow_cards = trello.boards.get_card(ISSOW_BOARD_ID,None,None,None,None,None,"all","idList,name,url,labels,closed")
+all_cards = issow_board.all_cards( )
+for card in all_cards:
+    incident_number = card.name.partition('-')[0].strip()
     xref_dict[incident_number] = card 
 
-    if incident_number not in servicenow_dump['dict'] and card['idList'] != CLOSED_LIST_ID:
-        print("%s missing in service now export - list %s" % (incident_number, list_lookup[card['idList']]['name']))
+    if incident_number not in servicenow_dump['dict'] and card.idList != CLOSED_LIST_ID:
+        print("%s missing in service now export - list %s" % (incident_number, list_lookup[card.idList].name))
         continue
 
-    if card['closed'] == True:
+    if card.closed == True:
         continue
 
-    if card['idList'] == CLOSED_LIST_ID:
+    if card.idList == CLOSED_LIST_ID:
         if servicenow_dump['dict'][incident_number]['incident_state'] not in {'Resolved', 'Cancelled'}:
             print("%s closed in Trello, Status in SN = %s" % 
                 (incident_number, servicenow_dump['dict'][incident_number]['incident_state']))
@@ -58,6 +62,6 @@ for record in servicenow_dump['rows']:
     if (record['number'] not in xref_dict):
         print("%s missing in trello" % (record['number']))
         continue
-
-    if record['incident_state'] in {'Resolved', 'Cancelled'} and xref_dict[record['number']]['idList'] != CLOSED_LIST_ID:
-        print("%s status mismatch - " % (record['number']))
+    list_id = xref_dict[record['number']].idList
+    if record['incident_state'] in {'Resolved', 'Cancelled'} and list_id != CLOSED_LIST_ID:
+        print("%s status mismatch - %s / %s" % (record['number'], record['incident_state'], list_lookup[list_id].name))

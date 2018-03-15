2477030cb4587aa1cd2d8ee6f5fa19cecbd0b885
Nick Fajardo <Nick.Fajardo@conocophillips.com>
2018-03-15 16:45:22 -0500

    Move Trello constants into separate file, prep to invalidate API keys

--- python-tools/add_to_trello.py
+++ python-tools/add_to_trello.py
@@ -2,37 +2,20 @@ from trello import TrelloClient
 from trello import Label as TrelloLabel
 from pprint import pprint
 from inspect import getmembers
+from trello_constants import *
+
 
 import pyperclip
 
-TRELLO_IDS = type('trello_ids', 
-    (object,), {
-        "BOARDS" : type('board_ids',(object,), { "BACKLOG" : '587d4739aacd90eb33fedfce', "INWORK" : '59b801ead00a15fd62d1fe05' } ),
-        "LISTS"  : type('list_ids' ,(object,), { "NEWLY_CREATED" : '587e244b50ed39d516edf94f', "NOT_BVILLE" : '598b14d18ce45e0e70ace903', "RESOLVED" : '587fb500aa3ee04f092ccc7d'} ),
-        "LABELS" : type('label_ids',(object,), { "BU_PRIORITY" : '58823fceced82109ffdda066',"CONFIG" : '58a46d1eced82109ff4b1165' } )
-        }
-    )
-
-BU_TRELLO_LABELS = {
-     "AP": { "name" : "[AP]LNG",   "id": '5909f067ced82109ff73e5e1', "label_obj":None },
-     "AL": { "name" : "[Al]pine",  "id": '587d4927edd7e54c19c1537d', "label_obj":None },
-     "B" : { "name" : "[B]Ville",  "id": '587d4928c71ad05630e1700a', "label_obj":None },
-     "D" : { "name" : "[D]arwin",  "id": '587d4927c718b5056ccd5c57', "label_obj":None },
-     "J" : { "name" : "[J]Area",   "id": '587d492828c911be88e947aa', "label_obj":None },
-     "N" : { "name" : "[N]orway",  "id": '587d4928e58413b6db42f672', "label_obj":None },
-     "S" : { "name" : "[S]uban",   "id": '587d4927ae03f523db5922e8', "label_obj":None },
-     "SU": { "name" : "[Su]rmont", "id": '587d4926323a283815b8947c', "label_obj":None },
-     "T" : { "name" : "[T]eeside", "id": '587d4926bfe0ba786c54918c', "label_obj":None } }
-
 client = TrelloClient(
-    api_key='***REMOVED***',
-    api_secret='***REMOVED***',
-    token='***REMOVED***',
-    token_secret='***REMOVED***'
+    api_key=TRELLO_SECRETS['api_key'],
+    api_secret=TRELLO_SECRETS['api_secret'],
+    token=TRELLO_SECRETS['token'],
+    token_secret=TRELLO_SECRETS['token_secret']
 )
 
 bu_names = []
-for bu in BU_TRELLO_LABELS.items() :
+for bu in BU_LABELS.items() :
     bu_names.append( bu[1]["name"] )
 
 list_created = client.get_board(TRELLO_IDS.BOARDS.BACKLOG).get_list(TRELLO_IDS.LISTS.NEWLY_CREATED)
@@ -57,10 +40,10 @@ def create_trello_ticket():
 
     while True:
         bu_input = input("** Business Unit?\n%s - " % (', '.join(bu_names))).upper()
-        if bu_input not in list(BU_TRELLO_LABELS):
+        if bu_input not in list(BU_LABELS):
             print("BU not recognized.  Try again...\n")
         else:
-            bu_label = BU_TRELLO_LABELS[bu_input]
+            bu_label = BU_LABELS[bu_input]
             break
     
     while True:
--- python-tools/match_trello_to_SN.py
+++ python-tools/match_trello_to_SN.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import json
 import csv
 from os import path
+from trello_secrets import *
 
 def read_servicenow_file():
     file_rows = []
@@ -18,23 +19,19 @@ def read_servicenow_file():
     return { 'rows' : file_rows, 'dict' : sn_dict }
 
 client = TrelloClient(
-    api_key='***REMOVED***',
-    api_secret='***REMOVED***',
-    token='***REMOVED***',
-    token_secret='***REMOVED***'
+    api_key=TRELLO_SECRETS['api_key'],
+    api_secret=TRELLO_SECRETS['api_secret'],
+    token=TRELLO_SECRETS['token'],
+    token_secret=TRELLO_SECRETS['token_secret']
 )
-BACKLOG_BOARD_ID = '587d4739aacd90eb33fedfce'
-INWORK_BOARD_ID = '59b801ead00a15fd62d1fe05'
-
-CLOSED_LIST_ID = '587fb500aa3ee04f092ccc7d'
 
 xref_dict = {}
 list_lookup = {}
 
 servicenow_dump = read_servicenow_file()
 
-backlog_board = client.get_board(BACKLOG_BOARD_ID)
-inwork_board = client.get_board(INWORK_BOARD_ID)
+backlog_board = client.get_board(TRELLO_IDS.BOARDS.BACKLOG)
+inwork_board = client.get_board(TRELLO_IDS.BOARDS.INWORK)
 
 issow_lists = inwork_board.all_lists( ) + backlog_board.all_lists( )
 
@@ -47,14 +44,14 @@ for card in all_cards:
     incident_number = card.name.partition('-')[0].strip()
     xref_dict[incident_number] = card 
 
-    if incident_number not in servicenow_dump['dict'] and card.idList != CLOSED_LIST_ID:
+    if incident_number not in servicenow_dump['dict'] and card.idList != TRELLO_IDS.LISTS.RESOLVED:
         print("%s missing in service now export - list %s" % (incident_number, list_lookup[card.idList].name))
         continue
 
     if card.closed == True:
         continue
 
-    if card.idList == CLOSED_LIST_ID and incident_number in servicenow_dump['dict']:
+    if card.idList == TRELLO_IDS.LISTS.RESOLVED and incident_number in servicenow_dump['dict']:
         if servicenow_dump['dict'][incident_number]['incident_state'] not in {'Resolved', 'Cancelled'}:
             print("%s closed in Trello, Status in SN = %s" % (incident_number, servicenow_dump['dict'][incident_number]['incident_state']))
            
@@ -65,5 +62,5 @@ for record in servicenow_dump['rows']:
         print("%s missing in trello" % (record['number']))
         continue
     list_id = xref_dict[record['number']].idList
-    if record['incident_state'] in {'Resolved', 'Cancelled'} and list_id != CLOSED_LIST_ID:
+    if record['incident_state'] in {'Resolved', 'Cancelled'} and list_id != TRELLO_IDS.LISTS.RESOLVED:
         print("%s status mismatch - %s / %s" % (record['number'], record['incident_state'], list_lookup[list_id].name))

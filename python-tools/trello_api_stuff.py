b64f10bc9263029364a78c06f538555a741022e6
Nick Fajardo <Nick.Fajardo@conocophillips.com>
2018-03-16 10:35:54 -0500

    Separate file for API key information

---
+++ python-tools/trello_api_stuff.py
@@ -0,0 +1,9 @@
+#Log in at Trello.com, then go to https://trello.com/app-key
+
+#API Key is near the top of the page, api_secret near the bottom (in the OAuth1 section)
+#The token is generated via the "Token" link on that same page
+
+TRELLO_SECRETS = {
+    'api_key':      '',
+    'api_secret':   '',
+    'token':        '' }
